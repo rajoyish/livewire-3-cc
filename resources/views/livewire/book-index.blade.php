aac10717920aae600535e7a6d5e90269d1e472a3
Rajesh Budhathoki <rajoyish@gmail.com>
2023-10-24 19:03:33 +0545

    Diving straight into forms

--- resources/views/livewire/book-index.blade.php
+++ resources/views/livewire/book-index.blade.php
@@ -1,5 +1,16 @@
-<div class="space-y-2">
-    @foreach ($books as $book)
-        <livewire:book-item :book="$book" wire:key="$book" /> 
-    @endforeach
-</div>
+<d iv>
+    <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
+        <div class="p-6 text-gray-900">
+            {{ $title }}
+            <form wire:submit.prevent="createBook">
+                <input type="text" id="title" wire:model="title">
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    </div>
+    <div class="space-y-2">
+        @foreach ($books as $book)
+            <livewire:book-item :book="$book" wire:key="$book" />
+        @endforeach
+    </div>
+    </div>
