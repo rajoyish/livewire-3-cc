5df99df82c1eab8c39f7a04fdaa99739db221e50
Rajesh Budhathoki <rajoyish@gmail.com>
2023-10-24 14:27:23 +0545

    Nesting and keying components

--- resources/views/livewire/book-index.blade.php
+++ resources/views/livewire/book-index.blade.php
@@ -1,11 +1,5 @@
 <div class="space-y-2">
     @foreach ($books as $book)
-        <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
-            <div class="p-6 text-gray-900">
-                <h2 class="text-lg font-bold leading-snug">{{ $book->title }}</h2>
-                <p>by {{ $book->author }}</p> 
-                <p class="mt-4">{{ $book->notes }}</p>
-            </div>
-        </div>
+        <livewire:book-item :book="$book" wire:key="$book" /> 
     @endforeach
 </div>
---
+++ resources/views/livewire/book-item.blade.php
@@ -0,0 +1,7 @@
+<div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
+    <div class="p-6 text-gray-900">
+        <h2 class="text-lg font-bold leading-snug">{{ $book->title }}</h2>
+        <p>by {{ $book->author }}</p> 
+        <p class="mt-4">{{ $book->notes }}</p>
+    </div>
+</div>
