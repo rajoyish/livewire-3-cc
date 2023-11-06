c473b5128f4d8847592fb2f913aa1044f0532920
Rajesh Budhathoki <rajoyish@gmail.com>
2023-11-06 20:47:14 +0545

    Adding a search component with URL query parameters

--- app/Livewire/BookIndex.php
+++ app/Livewire/BookIndex.php
@@ -4,12 +4,16 @@ namespace App\Livewire;
 
 use App\Models\Book;
 use Livewire\Attributes\On;
+use Livewire\Attributes\Url;
 use Livewire\Component;
 use Livewire\WithPagination;
 
 class BookIndex extends Component
 {
     use WithPagination;
+
+    #[Url(as: 'q')]
+    public string $query = '';
 
     // #[On('book.created')]
     // public function bookCreated(int $bookId)
@@ -33,7 +37,14 @@ class BookIndex extends Component
     public function render()
     {
         return view('livewire.book-index', [
-            'books' => auth()->user()->books()->latest()->paginate(3),
+            'books' => auth()->user()
+                ->books()
+                ->when(filled($this->query), function ($query) {
+                    $query->where('title', 'LIKE', '%'.$this->query.'%')
+                        ->orWhere('author', 'LIKE', '%'.$this->query.'%');
+                })
+                ->latest()
+                ->paginate(2),
         ]);
     }
 }
--- resources/views/livewire/book-index.blade.php
+++ resources/views/livewire/book-index.blade.php
@@ -1,10 +1,22 @@
 <div class="space-y-4">
-    @if ($books->count())
-        <div class="space-y-2" id="books">
-            @foreach ($books as $book)
-                <livewire:book-item :book="$book" wire:key="{{ $book }}" />
-            @endforeach
+    <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
+        <div class="p-6 text-gray-900 space-y-2"> 
+            <div class="relative flex-auto">
+                <x-input-label for="query" class="sr-only"> Search</x-input-label>
+                <x-text-input wire:model.live.debounce.300ms="query" type="search" id="query" class="w-full pl-10"
+                    placeholder="Search your books" />
+                <span class="absolute inset-y-0 left-0 grid place-items-center ml-2"><x-search-icon /></span>
+            </div>
         </div>
-        {{ $books->links(data: ['scrollTo' => '#books']) }}
-    @endif
+    </div>
+    <div class="space-y-4">
+        @if ($books->count())
+            <div class="space-y-2" id="books">
+                @foreach ($books as $book)
+                    <livewire:book-item :book="$book" wire:key="{{ $book }}" />
+                @endforeach
+            </div>
+            {{ $books->links(data: ['scrollTo' => '#books']) }}
+        @endif
+    </div>
 </div>
