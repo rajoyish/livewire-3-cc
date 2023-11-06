a00985699134ec7129339544b0a21ad4a764ddb5
Rajesh Budhathoki <rajoyish@gmail.com>
2023-11-06 17:36:19 +0545

    Calling Livewire from JavaScript

--- resources/views/livewire/book-item.blade.php
+++ resources/views/livewire/book-item.blade.php
@@ -7,6 +7,15 @@
         <p>by {{ $book->author }}</p>
         <p class="mt-4">{{ $book->notes }}</p>
         <x-secondary-button wire:click="$toggle('editing')">Edit</x-secondary-button>
-        <x-danger-button wire:click="$parent.deleteBook({{ $book->id }})">Delete</x-danger-button>
+
+        {{-- <x-danger-button wire:click="$parent.deleteBook({{ $book->id }})">Delete</x-danger-button> --}}
+
+        <x-danger-button x-data
+            x-on:click=" 
+            window.confirm('Are you sure?') 
+            ? $wire.$parent.deleteBook({{ $book->id }}) 
+            : '' ">
+            Delete
+        </x-danger-button>
     </div>
 </div>
